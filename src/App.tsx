f166c06748881d69ea7a33214466644366b638d9
lvest <ookkid.1@gmail.com>
2022-04-08 06:12:12 +0900

    초기 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Router from "./Router";
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+import { ThemeProvider } from "styled-components";
+import { ReactQueryDevtools } from "react-query/devtools";
+import { darkTheme, lightTheme } from "./theme";
+
+const GlobalStyle = createGlobalStyle`
+  
+  ${reset}
+  
+  *{
+    box-sizing: border-box;
+  }
+  body{
+    font-family: 'Source Sans Pro', sans-serif;
+    background-color: ${(props) => props.theme.bgColor};
+    color:${(props) => props.theme.textColor}
+  }
+  a{
+    text-decoration: none;
+    color:inherit;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ThemeProvider theme={darkTheme}>
+        <GlobalStyle />
+        <Router />
+      </ThemeProvider>
+    </>
   );
 }
 
