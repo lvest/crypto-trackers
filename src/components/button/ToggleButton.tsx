078f45d7eaa6ac518d27f5c05e23bee7bd26f622
lvest <ookkid.1@gmail.com>
2022-04-10 23:28:01 +0900

    feat: 토글 버튼 생성

---
+++ src/components/button/ToggleButton.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  width: 5rem;
+  height: 2rem;
+  border-radius: 1rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  background-color: ${(props) => props.theme.boxBgColor};
+  cursor: pointer;
+`;
+
+const Circle = styled.div`
+  width: 1.9rem;
+  height: 1.9rem;
+  margin-left: 1px;
+  border-radius: 50%;
+  background-color: ${(props) => props.theme.bgColor};
+  transition: all 0.2s;
+
+  &.clicked {
+    transform: translateX(calc(3rem));
+  }
+`;
+
+interface IToggleButton {
+  onClickFunction: () => void;
+}
+
+function ToggleButton({ onClickFunction }: IToggleButton) {
+  const [isClicked, setIsClicked] = useState(false);
+  const onClickHandler = () => {
+    setIsClicked(!isClicked);
+    onClickFunction();
+  };
+
+  return (
+    <Wrapper onClick={onClickHandler}>
+      <Circle className={isClicked ? "clicked" : ""}></Circle>
+    </Wrapper>
+  );
+}
+
+export default ToggleButton;
